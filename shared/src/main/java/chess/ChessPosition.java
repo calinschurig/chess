35f0638d0fa4f2239ce4366ba6cad68ad130220e
cschurig <calinschurig@gmail.com>
2024-07-01 10:05:15 -0600

    Added equals and hash to ChessPosition.java

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -31,5 +33,18 @@ public class ChessPosition {
     public int getColumn() {
         return Col;
         //throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPosition that = (ChessPosition) o;
+        return Row == that.Row && Col == that.Col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Row, Col);
     }
 }
