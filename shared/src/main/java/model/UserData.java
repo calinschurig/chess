0bcdffdb4b47ce31bac0616126386a1f01302fec
cschurig <calinschurig@gmail.com>
2024-07-23 13:42:28 -0600

    Added an Identifier interface to the Data record classes to make it easier to deal with with DAOs.

--- shared/src/main/java/model/UserData.java
+++ shared/src/main/java/model/UserData.java
@@ -2,5 +2,8 @@ package model;
 
 public record UserData(
         String username, String password, String email
-) {
+) implements Identifier<String> {
+    public String getId() {
+        return username;
+    }
 }
