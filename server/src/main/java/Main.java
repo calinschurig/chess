96487008901f699e70b720ab994eb7f3812f475e
cschurig <calinschurig@gmail.com>
2024-07-24 16:50:14 -0600

    Updated

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,11 +1,20 @@
 import chess.*;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryParentDAO;
+import dataaccess.MemoryUserDAO;
+import handler.Handler;
+import model.UserData;
 import server.Server;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
-        server.Server myServer = new server.Server();
+        server.Server myServer = new Server(new Handler(new MemoryUserDAO(), new MemoryGameDAO(), new MemoryAuthDAO()));
         myServer.run(8080);
+
+        MemoryParentDAO<String, UserData> userDAO = new MemoryParentDAO<>();
+
     }
 }
