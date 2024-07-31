15a1765db7f2cc0dcc2fd26256e4b8986231974c
Calin <calinschurig@gmail.com>
2024-07-31 15:57:21 -0600

    updated

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,9 +1,18 @@
 package model;
 
+import java.lang.reflect.Field;
+
 public record AuthData (
         String authToken, String username
 ) implements Identifier<String> {
     public String getId() {
         return authToken;
+    }
+    public Field getIdField() {
+        try {
+            return this.getClass().getDeclaredField("authToken");
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
